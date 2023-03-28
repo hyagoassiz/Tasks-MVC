cc3c9f0b26742ac16cec90d435838e6be2a375da
Hyago Assiz de Souza <hyagoassiz@hotmail.com>
2023-03-27 23:22:26 -0300

    ajustes

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -29,6 +29,25 @@ module.exports = class TaskController {
         res.render("tasks/edit", {task})
     }
 
+    static async updateTaskPost (req, res) {
+        const id = req.body.id
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+        }
+        await Task.update(task, { where: {id: id } })
+        res.redirect("/tasks")
+    }
+
+    static async toggleTaskStatus (req, res) {
+        const id = req.body.id
+        const task = {
+            done: req.body.done === "0" ? true : false,
+        }
+        await Task.update(task, { where: { id: id} })
+        res.redirect("/tasks")
+    }
+
     static async showTasks(req, res) {
         const tasks = await Task.findAll({ raw: true})
         res.render("tasks/all", {tasks})
